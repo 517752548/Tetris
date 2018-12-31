ef0192b630326c2263690f864afaa64955976f87
haydnkeung <haydnkeung@gmail.com>
2018-12-31 14:22:38 -0800

    Added Boundaries

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public GameObject[] list;//Collection of all Tetrimos
+    public int startX = 0;
+    public int endX = 10;
+    public int width = 0;
+    public int height = 0;
+
+
+
+    public void Start()
     {
-        
+        if (width == 0) {
+            width = (endX - startX) + 1;
+        }
+        if (height == 0) {
+            height = 20;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+
+    public void spawnNext() {
+
+        int index = Random.Range(0, list.Length);
+        Instantiate(list[index], new Vector3(5, 18, 0), Quaternion.identity);
     }
+
+    public bool isInGrid(Vector3 pos) {
+
+        if ((int)pos.x < startX || (int)pos.x > endX) {
+            return false;//Retrurns false if outside of x bounds
+        }
+        if ((int)pos.y < 0) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+
+
 }
--- Assets/Scripts/Tetrimo.cs
+++ Assets/Scripts/Tetrimo.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Tetrimo : MonoBehaviour
 {
+
+    public float fallspeed = 10;
+    public float time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,84 @@ public class Tetrimo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        checkUserInput();
     }
+
+
+    Vector3 round(Vector3 input) {
+        return new Vector3(Mathf.Round(input.x),Mathf.Round(input.y),Mathf.Round(input.z));
+
+    }
+
+
+    bool isValid(Transform tetrimo) {
+
+        foreach(Transform minimo in tetrimo){
+
+            Vector3 pos = round(minimo.position);
+
+            if (!FindObjectOfType<Game>().isInGrid(pos)) {
+                return false;
+            }
+            
+
+        }
+
+        return true;
+    }
+
+    void checkUserInput() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left;
+            if (isValid(transform))
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.right;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+            if (isValid(transform))
+            {
+
+            }
+            else
+            {
+                transform.position += Vector3.left;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+            transform.position = round(transform.position);
+            if (isValid(transform))
+            {
+
+            }
+            else
+            {
+                transform.Rotate(0, 0, -90);
+                transform.position = round(transform.position);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - time >= fallspeed) {
+            transform.position += Vector3.down;
+            if (isValid(transform))
+            {
+
+            }
+            else {
+                transform.position += Vector3.up;
+            }
+
+            time = Time.time;
+        }
+    }
+
+    
 }
